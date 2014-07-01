abb5dbe0ddcdacf7326a789a2e8bc160da319f9d
charley <yc@charles.young>
2014-07-01 23:55:33 +0800

    Mini Harp Static Server(2) --Implement The mini-harp Command

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,4 +1,6 @@
 #! /usr/bin/env node
+var argv = require('minimist')(process.argv.slice(2));
 var createMiniHarp = require('mini-harp'), app = createMiniHarp();
-console.log("Starting mini-harp on http://localhost:4000");
-app.listen(4000);
+var port = argv.port||4000;
+console.log("Starting mini-harp on http://localhost:"+port);
+app.listen(port);
