5b14d0f0dc3d2c6580677e4ef2de3071d9b0a637
charley <yc@charles.young>
2014-07-03 22:28:06 +0800

    Mini Harp Static Server(2) Implement A Current Time Middleware

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -2,5 +2,5 @@
 var argv = require('minimist')(process.argv.slice(2));
 var createMiniHarp = require('mini-harp'), app = createMiniHarp();
 var port = argv.port||4000;
-console.log("Starting mini-harp on http://localhost:"+port);
+console.log('Starting mini-harp on http://localhost:'+port);
 app.listen(port);
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var connect = require('connect');
-var createMiniHarp = (function() {
-  return connect;
-})();
+var createMiniHarp = function() {
+  var app = connect();
+  app.use(function(req, res, next) {
+    var url = req.url.split('/');
+    if (url[1] == 'current-time') {
+      res.end(new Date().toISOString() + '\n');
+    } else {
+      next();
+    }
+  });
+  return app;
+};
 
 module.exports = createMiniHarp;
